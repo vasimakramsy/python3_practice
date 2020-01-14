19a37dbe88e216881ca8d87097f74837b4c45911
Vasim Akram <50202400+vasimakramsy@users.noreply.github.com>
2020-01-14 22:46:38 +0530

    Add files via upload

---
+++ binarysearch.py
@@ -0,0 +1,20 @@
+
+# Iterative Binary Search 
+def binary_search_iterative(data, target):
+	low = 0
+	high = len(data) - 1
+
+	while low <= high:
+		mid = (low + high) // 2
+		if target == data[mid]:
+			return True
+		elif target < data[mid]:
+			high = mid - 1
+		else:
+			low = mid + 1
+	return False 
+
+
+print(binary_search_iterative([1,2,3,4,5],7))
+
+
